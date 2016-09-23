a048c73ea925570d43df4da6c8d3fe6452b78382
Tam Dang <dangtam@gmail.com>
2016-09-23 22:59:18 +0700

    Initial Commit

---
+++ dangkykhambenh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dangkykhambenh
+//
+//  Created by Tam Dang on 9/23/16.
+//  Copyright © 2016 Tam Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
